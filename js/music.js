9bf5915deebbee335e82cda6adaee7fe1844ca7e
FreshGame <88659066+FreshGame1@users.noreply.github.com>
2023-08-29 21:19:03 +1000

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,25 +1,12 @@
-var feedUrl = "https://api.boosty.to/v1/blog/freshgame.com/rss/feed/d660a0b4-d439-4c46-ae4f-b43ea476ac34";
+var tracks = [
+  { name: "Space Survival", url: "music/Space_Survival.mp3" }
+];
+var currentTrackIndex = 0;
 var audio = document.getElementById("music");
 var trackNameElement = document.getElementById("trackName");
-var currentTrackIndex = 0;
-var tracks = [];
-
-fetch(feedUrl)
-  .then(response => response.text())
-  .then(data => {
-    var parser = new DOMParser();
-    var xmlDoc = parser.parseFromString(data, "text/xml");
-    var items = xmlDoc.getElementsByTagName("item");
-    for (var i = 0; i < items.length; i++) {
-      var title = items[i].getElementsByTagName("title")[0].textContent;
-      var url = items[i].getElementsByTagName("enclosure")[0].getAttribute("url");
-      tracks.push({ name: title, url: url });
-    }
-    updateTrackName();
-  });
 
 function updateTrackName() {
- // trackNameElement.textContent = tracks[currentTrackIndex].name;
+  trackNameElement.textContent = tracks[currentTrackIndex].name;
 }
 
 function playPause() {
