f9ce0a0169797b305da791b636d15f48f3367d10
FreshGame <88659066+FreshGame1@users.noreply.github.com>
2023-09-19 17:36:29 +1000

    Update studio.js

--- js/studio.js
+++ js/studio.js
@@ -1,22 +1,14 @@
-// Устанавливаем начальную дату
-var startDate = new Date("2021.06.09");
+ // Устанавливаем начальную дату
+  var startDate = new Date("2021.06.09");
 
-// Получаем текущую дату
-var currentDate = new Date();
+  // Получаем текущую дату
+  var currentDate = new Date();
 
-// Вычисляем количество прошедших лет
-var yearsPassed = currentDate.getFullYear() - startDate.getFullYear();
+  // Вычисляем количество прошедших лет
+  var yearsPassed = currentDate.getFullYear() - startDate.getFullYear();
 
-// Вычисляем значение счетчика
-var counterValue = 0 + yearsPassed;
+  // Вычисляем значение счетчика
+  var counterValue = 0 + yearsPassed;
 
-// Получаем текущий год, месяц и день
-var currentYear = currentDate.getFullYear();
-var currentMonth = currentDate.getMonth() + 1; // добавляем 1, так как месяцы в JavaScript начинаются с 0
-var currentDay = currentDate.getDate();
-
-// Обновляем текст в элементе с id "counter" и отображаем текущий год, месяц и день
-document.getElementById("counter").textContent = counterValue;
-document.getElementById("year").textContent = currentYear;
-document.getElementById("month").textContent = currentMonth;
-document.getElementById("day").textContent = currentDay;
+  // Обновляем текст в элементе с id "counter"
+  document.getElementById("counter").textContent = counterValue;
