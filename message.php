ab4b90946d52f4e2a81743f7a4e9742008479e70
FreshGame <88659066+FreshGame1@users.noreply.github.com>
2023-10-04 15:46:53 +1000

    Update message.php

--- message.php
+++ message.php
@@ -1,9 +1,9 @@
 <?php
-$app_name   = $_POST[“app”];
-$sender     = $_POST[“sender”];
-$message    = $_POST[“message”];
-$phone      = $_POST["phone"];
-$group_name = $_POST["group_name"];
+$app_name   = $_POST[“WhatsAuto”];
+$sender     = $_POST[“WhatsAuto app”];
+$message    = $_POST[“Тестовое сообщение”];
+$phone      = $_POST["+79147769237"];
+$group_name = $_POST["🛡🗡FreshLend🗡🛡"];
 
 $response = array("reply" => "Hello $sender, we received your message $message.");
 echo json_encode($response);
